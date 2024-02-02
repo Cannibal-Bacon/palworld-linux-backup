35370cad72cf9f06a3fcb2c8b4a67393269ab5ae
Cannibal-Bacon <126197078+Cannibal-Bacon@users.noreply.github.com>
2024-02-02 00:03:18 -0500

    Update pwbackup.sh
    Cleaned up

--- pwbackup.sh
+++ pwbackup.sh
@@ -3,9 +3,13 @@
 ## cd /DIRECTORY/TO/SAVE/BACKUP/IN
 cd /home/deck/backups/pw/
 
-## ftp://USERNAME:PASSWORD@HOST/PATH/TO/FILESYSTEM
+## Add Username, Password, and Host here.
+USERNAME=username
+PASSWORD=password
+HOST=127.0.0.1
+
 ## number after --cut-dirs= can be changed depending on depth of filesystem you wish to copy
-wget -m -nH --cut-dirs=5 ftp://USERNAME:PASSWORD@HOST/server-data/Pal/Saved/SaveGames/0/
+wget -m -nH --cut-dirs=5 ftp://$USERNAME:$PASSWORD@$HOST/server-data/Pal/Saved/SaveGames/0/
 
 exit
 
